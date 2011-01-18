16297f8c7d48d477bc9abc5b06d4e246a9a97ed0
Ben Marini <bmarini@gmail.com>
2011-01-17 18:05:47 -0800

    Add a pool test

--- test/lib/poolparty/pool_test.rb
+++ test/lib/poolparty/pool_test.rb
@@ -2,11 +2,26 @@ require "test_helper"
 
 stub_keypair_searchable_paths
 
-class PoolTest < Test::Unit::TestCase  
-  def test_set_up_pool_object
-    reset!
-    pool "hi" do
+class PoolTest < Test::Unit::TestCase
+  context "Pool" do
+    setup do
+      reset!
     end
-    assert_equal @@pool.name, "hi"
+
+    should "create a global pool object" do
+      pool "hi" do
+      end
+      assert_equal @@pool.name, "hi"
+    end
+
+    should "create a cloud" do
+      pool = PoolParty::Pool.new :test
+      pool.cloud "project1" do
+        using :ec2
+      end
+
+      assert_instance_of PoolParty::Cloud, pool.clouds["project1"]
+    end
+
   end
 end
